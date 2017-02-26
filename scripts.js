db70969a1b3cb400286dc06d180aa79766f1a1cd
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2017-02-26 00:04:20 -0500

    Updating Scripts 3

--- scripts.js
+++ scripts.js
@@ -38,24 +38,34 @@ $(document).ready(function() {
 
   function anxietyVideos(){
 
-    var anxietyVid = '<h3> 10 Most Common Anxiety Symptoms - Mental Health </h3>'+
+    var anxietyVids = '<h3> 10 Most Common Anxiety Symptoms - Mental Health </h3>'+
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/SDPW3pdlnLk" frameborder="0" allowfullscreen></iframe>'+
-      '<h3> HOW TO OVERCOME DEPRESSION | ANXIETY | HARD TIMES</h3>'+
+      '</br><h3> HOW TO OVERCOME DEPRESSION | ANXIETY | HARD TIMES</h3>'+
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/kKcRSboPgU4" frameborder="0" allowfullscreen></iframe>'
 
-    return anxietyVid1;
+    return anxietyVids;
   }
 
   function depressionVideos(){
-  		
+  	var depressionVids = '<h3> What’s It Like To Have Bipolar Disorder</h3>'+
+						 '<iframe width="560" height="315" src="https://www.youtube.com/embed/2rvQQiQN-Pg" frameborder="0" allowfullscreen></iframe>'+
+						 '</br><h3>TEDxTerryTalks - Laura Bain - Living with Bipolar Type II</h3>'+
+   						 '<iframe width="560" height="315" src="https://www.youtube.com/embed/8Ki9dgG3P5M" frameborder="0" allowfullscreen></iframe>'
+   	return depressionVids;
   }
 
   function addictionVideos(){
-
+  	var addictionVids = 'h3 Everything you think you know about addiction is wrong | Johann Hari></h3>'+
+					  '<iframe width="560" height="315" src="https://www.youtube.com/embed/PY9DcIMGxMs" frameborder="0" allowfullscreen></iframe>'+
+					  '</br><h3 The Connections Between Eating Disorders and Addictions></h3>'+
+					  '<iframe width="560" height="315" src="https://www.youtube.com/embed/y6FTwzm5zYY" frameborder="0" allowfullscreen></iframe>'
+	return addictionVids;
   }
 
   function didVideos(){
-
+  var didVids = '<h3>What is Dissociative Identity Disorder? Multiple Personalities | Kati Morton treatment trauma did></h3>'+
+				'<iframe width="560" height="315" src="https://www.youtube.com/embed/BoJNkKWs3iI" frameborder="0" allowfullscreen></iframe>'
+ 	return didVideos;
   }
 
   function videos(illness){
@@ -114,18 +124,21 @@ $("#zip-submit").click(function(){
       scrollDown();
       var illness = 'depression';
       groups(illness);
+      videos(illness);
   });
 
   $("#addictionBtn").click(function(){
       scrollDown();
       var illness = 'addiction';
       groups(illness);
+      videos(illness);
   });
 
   $("#didBtn").click(function(){
       scrollDown();
       var illness = 'did';
       groups(illness);
+      videos(illness);
   });
 
 });
